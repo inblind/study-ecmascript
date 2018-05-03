6ffa417211704282ce8ab1c210c73ce8ca5c92c5
Rafael Montero <rafaelmonterov@gmail.com>
2018-05-03 17:49:47 -0300

    add some other files

---
+++ typeofvariables.js
@@ -0,0 +1,7 @@
+document.write("true: " + typeof(true) + "<br/>");
+document.write("1: " + typeof(1) + "<br/>");
+document.write("[1,2,3]: " + typeof([1,2,3]) + "<br/>");
+document.write("{a:1}" + typeof({a:1}) + "<br/>");
+document.write("undefined: " + typeof(undefined) + "<br/>");
+document.write("Symbol(): " + typeof(Symbol()) + "<br/>");
+document.write("string: " + typeof("Symbol()") + "<br/>");
